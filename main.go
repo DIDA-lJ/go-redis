beeb553b54a0623b5e6c369ec977701542cf79a8
linqi <wkw4399xs@gmail.com>
2023-12-05 17:05:36 +0800

    The 8 th commit: go-redis: main 方法编写

--- main.go
+++ main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"go-redis/config"
+	"go-redis/lib/logger"
+	"go-redis/tcp"
+	EchoHandler "go-redis/tcp"
+	"os"
+)
 
+const configFile string = "redis.conf"
+
+var defaultProperties = &config.ServerProperties{
+	Bind: "0.0.0.0",
+	Port: 6379,
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	return err == nil && !info.IsDir()
+}
+
+func main() {
+	logger.Setup(&logger.Settings{
+		Path:       "logs",
+		Name:       "go-redis",
+		Ext:        "log",
+		TimeFormat: "2023-12-05",
+	})
+
+	if fileExists(configFile) {
+		config.SetupConfig(configFile)
+	} else {
+		config.Properties = defaultProperties
+	}
+
+	err := tcp.ListenAndServeWithSignal(
+		&tcp.Config{
+			Address: fmt.Sprintf("%s:%d",
+				config.Properties.Bind,
+				config.Properties.Port),
+		},
+		EchoHandler.MakeHandler())
+	if err != nil {
+		logger.Error(err)
+	}
 }
