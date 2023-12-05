4f2d1dcb0787f4b1c51f2356fe61a577370eb6bb
linqi <wkw4399xs@gmail.com>
2023-12-05 16:30:53 +0800

    The 6 th commit: go-redis: 实现完善的 TCP 服务器。

--- interface/tcp/hadler.go
+++ interface/tcp/hadler.go
@@ -5,7 +5,7 @@ import (
 	"net"
 )
 
-// Handler /*  Handler 接口
+// Handler /*  Handler 接口，用于处理 TCP 连接，可以使得在写 TCP 连接的时候忽视 TCP 连接
 type Handler interface {
 	Handler(ctx context.Context, conn net.Conn)
 	Close() error
--- tcp/server.go
+++ tcp/server.go
@@ -5,6 +5,10 @@ import (
 	"go-redis/interface/tcp"
 	"go-redis/lib/logger"
 	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
 // Config 启动 Tcp Server 的结构体配置
@@ -13,9 +17,20 @@ type Config struct {
 }
 
 // ListenAndServerWithSignal 监听服务，如果有异常返回信号
-func ListenAndServerWithSignal(cfg *Config,
+func ListenAndServerWithSignal(
+	cfg *Config,
 	handler tcp.Handler) error {
+
 	closeChan := make(chan struct{})
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		sig := <-sigChan
+		switch sig {
+		case syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+			closeChan <- struct{}{}
+		}
+	}()
 	listener, err := net.Listen("tcp", cfg.Address)
 	if err != nil {
 		return err
@@ -29,13 +44,31 @@ func ListenAndServerWithSignal(cfg *Config,
 }
 
 // ListenAndServer  实现监听并且连接
-func ListenAndServer(listener net.Listener,
+func ListenAndServer(
+	listener net.Listener,
 	handler tcp.Handler,
+	// 防止用户在客户端未关闭就将进程杀死，感知系统信号
 	closeChan <-chan struct{}) {
 
+	go func() {
+		// 实现对于系统系统的监听
+		<-closeChan
+		// 收到来自系统的信号
+		logger.Info("shutting down")
+		_ = listener.Close()
+		_ = handler.Close()
+	}()
+
+	defer func() {
+		// 在栈中对于 listener 和 handler 方法实现关闭逻辑
+		_ = listener.Close()
+		_ = handler.Close()
+	}()
 	// 从 background 方法获取到一个上下文对象
 	ctx := context.Background()
+	var waitDone sync.WaitGroup
 
+	// 死循环接收连接错误
 	for true {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -43,8 +76,12 @@ func ListenAndServer(listener net.Listener,
 		}
 		logger.Info("accepted link")
 		go func() {
+			defer func() {
+				// 如果发生异常，会等待所有用户退出之后进行退出
+				waitDone.Done()
+			}()
 			handler.Handler(ctx, conn)
 		}()
 	}
-
+	waitDone.Wait()
 }
