dc41edb7261956e9eaef19600a25e400293fd1e0
linqi <wkw4399xs@gmail.com>
2023-12-09 14:56:08 +0800

    The 12 th commit: go-redis: 回复方案实现

--- resp/reply/reply.go
+++ resp/reply/reply.go
@@ -1,3 +1,4 @@
+// Package reply 一般的回复
 package reply
 
 import (
@@ -10,17 +11,20 @@ var (
 	nullBulkReplyBytes = []byte("$-1")
 
 	// CRLF is the line separator of redis serialization protocol
+	// 结尾回复字符串
 	CRLF = "\r\n"
 )
 
 /* ---- Bulk Reply ---- */
 
 // BulkReply stores a binary-safe string
+// 回复一个字符串，将一个不符合 Redis 协议的回复封装成符合 Redis 协议的回复
 type BulkReply struct {
 	Arg []byte
 }
 
 // MakeBulkReply creates  BulkReply
+// 输入字节，返回值是一个非空的回复
 func MakeBulkReply(arg []byte) *BulkReply {
 	return &BulkReply{
 		Arg: arg,
@@ -38,6 +42,7 @@ func (r *BulkReply) ToBytes() []byte {
 /* ---- Multi Bulk Reply ---- */
 
 // MultiBulkReply stores a list of string
+// 多个字符串的回复
 type MultiBulkReply struct {
 	Args [][]byte
 }
@@ -50,6 +55,7 @@ func MakeMultiBulkReply(args [][]byte) *MultiBulkReply {
 }
 
 // ToBytes marshal redis.Reply
+// 返回多个字符串的回复
 func (r *MultiBulkReply) ToBytes() []byte {
 	argLen := len(r.Args)
 	var buf bytes.Buffer
@@ -133,6 +139,7 @@ func MakeErrReply(status string) *StandardErrReply {
 }
 
 // IsErrorReply returns true if the given reply is error
+// 判断回复内容的第一个成员是不是减号
 func IsErrorReply(reply resp.Reply) bool {
 	return reply.ToBytes()[0] == '-'
 }
